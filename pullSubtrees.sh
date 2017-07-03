d003758a343534ce5c8c1c100b9da7b1b6e7781b
Dale Owens <physicali2001@yahoo.com>
2017-07-03 12:17:40 -0700

    add rust.vim

--- pullSubtrees.sh
+++ pullSubtrees.sh
@@ -4,3 +4,4 @@ git subtree pull -P bundle/nerdtree https://github.com/scrooloose/nerdtree.git m
 git subtree pull -P bundle/vim-go https://github.com/fatih/vim-go.git master --squash
 git subtree pull -P bundle/vim-nerdtree-tabs https://github.com/jistr/vim-nerdtree-tabs.git master --squash
 git subtree pull -P bundle/vim-glsl https://github.com/tikhomirov/vim-glsl master --squash
+git subtree pull -P bundle/rust.vim https://github.com/rust-lang/rust.vim.git master --squash
