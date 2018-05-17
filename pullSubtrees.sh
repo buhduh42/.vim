071d653ac25bdda636b657cda927545efe8a2ef0
dowen00 <Dale.Owens@bhnetwork.com>
2018-05-17 11:51:41 -0700

    rust/vim-racer and salt

--- pullSubtrees.sh
+++ pullSubtrees.sh
@@ -5,3 +5,5 @@ git subtree pull -P bundle/vim-go https://github.com/fatih/vim-go.git master --s
 git subtree pull -P bundle/vim-nerdtree-tabs https://github.com/jistr/vim-nerdtree-tabs.git master --squash
 git subtree pull -P bundle/vim-glsl https://github.com/tikhomirov/vim-glsl master --squash
 git subtree pull -P bundle/rust.vim https://github.com/rust-lang/rust.vim.git master --squash
+git subtree pull -P bundle/salt-vim https://github.com/saltstack/salt-vim.git master --squash
+git subtree pull -P bundle/vim-racer https://github.com/racer-rust/vim-racer.git master --squash
