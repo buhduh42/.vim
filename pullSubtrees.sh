5237c11502e2449155afa6f511d1ae5cb12d4222
Dale Owens <dale.owens@truelook.com>
2023-07-11 13:36:28 -0700

    blarg

--- pullSubtrees.sh
+++ pullSubtrees.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 #Run from .vim directory
-git subtree pull -P bundle/nerdtree https://github.com/scrooloose/nerdtree.git master --squash
-git subtree pull -P bundle/vim-go https://github.com/fatih/vim-go.git master --squash
-git subtree pull -P bundle/vim-nerdtree-tabs https://github.com/jistr/vim-nerdtree-tabs.git master --squash
-git subtree pull -P bundle/vim-glsl https://github.com/tikhomirov/vim-glsl master --squash
-git subtree pull -P bundle/rust.vim https://github.com/rust-lang/rust.vim.git master --squash
-git subtree pull -P bundle/salt-vim https://github.com/saltstack/salt-vim.git master --squash
-git subtree pull -P bundle/vim-racer https://github.com/racer-rust/vim-racer.git master --squash
-git subtree pull -P bundle/Jenkinsfile-vim-syntax https://github.com/martinda/Jenkinsfile-vim-syntax.git master --squash
-git subtree pull -P bundle/ctrlp https://github.com/kien/ctrlp.vim.git master --squash
-#git subtree pull -P bundle/omnisharp-vim git://github.com/OmniSharp/omnisharp-vim master --squash
-git subtree pull -P bundle/vim-indentwise https://github.com/jeetsukumaran/vim-indentwise.git master --squash
-git subtree pull -P bundle/vim-webapi git@github.com:mattn/webapi-vim.git master --squash
-git subtree pull -P bundle/typescript-vim https://github.com/leafgarland/typescript-vim.git master --squash
+repos=(\
+  https://github.com/scrooloose/nerdtree.git \
+  https://github.com/fatih/vim-go.git \
+  https://github.com/jistr/vim-nerdtree-tabs.git \
+  https://github.com/tikhomirov/vim-glsl \
+  https://github.com/rust-lang/rust.vim.git \
+  https://github.com/saltstack/salt-vim.git \
+  https://github.com/racer-rust/vim-racer.git \
+  https://github.com/martinda/Jenkinsfile-vim-syntax.git \
+  https://github.com/kien/ctrlp.vim.git \
+  https://github.com/jeetsukumaran/vim-indentwise.git \
+  git@github.com:mattn/webapi-vim.git \
+  https://github.com/leafgarland/typescript-vim.git \
+)
+for repo in ${repos[@]}; do
+  dir=$(echo -n ${repo%.*} | rev | cut -d'/' -f1 - | rev)
+  if ! test -e bundle/${dir}; then
+    git subtree add -P bundle/${dir} ${repo} master --squash
+  else
+    git subtree pull -P bundle/${dir} ${repo} master --squash
+  fi
+done
